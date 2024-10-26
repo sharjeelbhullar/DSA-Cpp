50c402bc71b2a03f85a35b86c6ba60c0404ea6d8
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-26 07:13:56 +0500

    Add 8 Patterns

--- patterns.cpp
+++ patterns.cpp
@@ -38,10 +38,56 @@ using namespace std;
 //     }
 // }
 
-void print5(int n){
-    for(int i=n; i>=1; i--){
-        for(int j=i; j>=1; j--){
+// void print5(int n){
+//     for(int i=n; i>=1; i--){
+//         for(int j=i; j>=1; j--){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print6(int n){
+//     for(int i=n; i >= 1; i--){
+//         for(int j=1; j<=i; j++){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print7(int n){
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             cout << " ";
+//         }
+//         for(int j=0; j<2*i+1; j++)
+//         {
+//             cout << "*";
+//         }
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+void print8(int n){
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            cout << "-";
+        }
+        for(int j=0; j<2*n-(2*i+1); j++)
+        {
             cout << "*";
+        }
+        for(int j=0; j<i; j++){
+            cout << "-";
         }
         cout << endl;
     }
@@ -54,11 +100,9 @@ int main() {
         int n;
         cout << "Number of lines of pattern: " << endl;
         cin >> n;
-        // print1(n);
-        // print2(n);
-        // print3(n);
-        // print4(n);
-        print5(n);
+         
+        print8(n);
     }
+
 
 }
