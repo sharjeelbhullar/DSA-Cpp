346c1fb130b87e0fa14bdf498d51fa443a743633
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-26 11:10:23 +0500

    Add more patterns by using functions

--- patterns.cpp
+++ patterns.cpp
@@ -75,23 +75,114 @@ using namespace std;
 //     }
 // }
 
-void print8(int n){
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<i; j++)
-        {
-            cout << "-";
-        }
-        for(int j=0; j<2*n-(2*i+1); j++)
-        {
-            cout << "*";
-        }
-        for(int j=0; j<i; j++){
-            cout << "-";
+// void print8(int n){
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for(int j=0; j<2*n-(2*i+1); j++)
+//         {
+//             cout << "*";
+//         }
+//         for(int j=0; j<i; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print9(int n){
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             cout << " ";
+//         }
+//         for(int j=0; j<2*i+1; j++)
+//         {
+//             cout << "*";
+//         }
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for(int j=0; j<2*n-(2*i+1); j++)
+//         {
+//             cout << "*";
+//         }
+//         for(int j=0; j<i; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print10(int n){
+//     for(int i = 1; i<=2*n-1; i++){
+//         int stars = i;
+//         if(i > n) stars = 2*n-i;
+//         for(int j=1; j<=stars; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int start = 1;
+// void print11(int n){
+//     for(int i=0; i<n; i++){
+//         if(i%2 == 0) start = 1;
+//         else start = 0;
+//         for(int j=0; j<=i; j++){
+//             cout << start;
+//             start = 1-start;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print12(int n){
+//     int space = 2*(n-1);
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << j;
+//         }
+//
+//         for(int j=1; j<=space; j++){
+//             cout << " ";
+//         }
+//
+//         for(int j=i; j>=1; j--){
+//             cout << j;
+//         }
+//         cout << endl;
+//         space -= 2;
+//     }
+// }
+
+void print13(int n){
+    int num = 1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << num << " ";
+            num += 1;
         }
         cout << endl;
     }
 }
+
+
+
 int main() {
     int t;
     cout << "Number of test cases: " << endl;
@@ -100,8 +191,7 @@ int main() {
         int n;
         cout << "Number of lines of pattern: " << endl;
         cin >> n;
-         
-        print8(n);
+        print13(n);
     }
 
 
