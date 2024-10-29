943a4b545d8e6600d78e6cb72798cfcf7273faed
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-29 08:57:42 +0500

    Add sets stl in cpp

---
+++ sets-stl.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    // Create a set called cars that will store strings
+    set<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    // Print set elements
+    for (string car : cars) {
+        cout << car << endl;
+    }
+
+    // Add new elements
+    cars.insert("Tesla");
+    cars.insert("VW");
+    cars.insert("Toyota");
+    cars.insert("Audi");
+
+    // Remove element
+    cars.erase("VW");
+
+    cout << "*****" << endl;
+
+    for (string car : cars) {
+        cout << car << "\n";
+    }
+
+    // Remove all elements
+    cars.clear();
+    cout << cars.size() << endl;
+    cout << "*****" << endl;
+    cout << cars.empty() << endl;
+
+    cout << "*****" << endl;
+
+    
+    //Create a set called numbers that will store integers
+    set<int> numbers = {1, 7, 3, 2, 5, 9};
+
+    // Print set elements
+    for (int num : numbers) {
+        cout << num << endl;
+    }
+
+    cout << "****" << endl;
+
+    //Sort elements in a set in descending order
+    set<int, greater<int>> arr = {1, 7, 3, 2, 5, 9};
+    //Print the elements
+    for (int num : arr) {
+        cout << num << endl;
+    }
+
+    return 0;
+
+}
