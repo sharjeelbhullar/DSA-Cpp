c40a4b4d4c5e21c94e4af7bff0c262f626b43f6b
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-11-01 18:29:05 +0500

    Add efficient time complexity function into divisors

--- Maths-Basics/divisors.cpp
+++ Maths-Basics/divisors.cpp
@@ -1,12 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//Time complexity is equal to O(N)
 void printDivisors(int N){
     cout << "Divisors are: ";
     for(int i=1; i<=N; i++){
         if(N % i == 0){
             cout << i << " ";        
         }
+    }
+    cout << endl;
+}
+//O(sqrt(N) x log(sqrt(N))) more efficient for larger values
+void getDivisors(int N){
+    vector<int> ls;
+    for(int i=1; i*i<=(N); i++){
+        if(N%i == 0){
+            ls.push_back(i);
+            if(N/i != i){
+                ls.push_back(N/i);
+            }
+        }
+    }
+    sort(ls.begin(), ls.end());
+
+    for(auto pr : ls){
+        cout << pr << " ";
     }
 }
 
@@ -15,6 +33,7 @@ int main() {
     cout << "Enter a number: " << endl;
     cin >> num;
     printDivisors(num);
+    getDivisors(num);
     return 0;
 
 }
