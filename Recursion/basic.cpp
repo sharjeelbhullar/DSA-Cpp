9aacc33cb0263293dbda02700cf06ab6f4023068
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-11-02 22:19:54 +0500

    Add basic recursion

---
+++ Recursion/basic.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void func(int i, int n){
+    if(i > n) return;
+    else{
+        cout << i << endl;
+    }
+
+    func(i+1, n);
+}
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    func(1, n);
+}
