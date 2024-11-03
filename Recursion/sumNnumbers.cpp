6938f7786d20f1dce9d98b5a9052ceb80fd69d61
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-11-03 13:01:38 +0500

    Add both parameterized and functional ways of print sum of N numbers

--- Recursion/sumNnumbers.cpp
+++ Recursion/sumNnumbers.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
+//parameterized way
 void sumOfNumbers(int n, int sum){
     if(n==0){
-        cout << "Sum -> " << sum;
+        cout << "Sum -> " << sum << endl;
         return;
     }
     //1 method
@@ -11,9 +12,17 @@ void sumOfNumbers(int n, int sum){
     //2 method
     //sumOfNumbers(n-1, sum+n);
 }
+
+//functional way
+int sumOfN(int n){
+    if(n==0) return 0;
+    return n + sumOfN(n-1);
+}
+
 int main(){
     int n;
     cout << "Enter a number: " << endl;
     cin >> n;
     sumOfNumbers(n, 0);
-} 
+    cout << "Functional Way: " << sumOfN(n);
+}
