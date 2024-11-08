c7c17f086efe057610bbce6ca605a38e3a5d60fc
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-11-08 18:49:58 +0500

    Add hashing using maps

---
+++ Basic-Hashing/Map/hashing.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the size of array: " << endl; 
+    cin >> n;
+    int arr[n];
+    map<int, int> mpp;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        //pre compute
+        mpp[arr[i]]++;
+    }
+    //pre compute
+    
+
+    //iterate in the map
+    // for(auto it : mpp) {
+    // cout << it. first << "->" << it.second << endl;
+    // }
+
+    int q;
+    cout << "Enter the value of q: " << endl;
+    cin >> q;
+    while(q--) {
+        int number;
+        cout << "Enter the value of number: " << endl;
+        cin >> number;
+        //fetch
+        cout << mpp[number] << endl;
+    }
+    return 0;
+}
