160bfe848c500f3bbec09ab8443a7c4e456c85ad
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-11-14 16:02:42 +0500

    Update

--- LeetCode/9. Palindrome Number.cpp
+++ LeetCode/9. Palindrome Number.cpp
@@ -16,31 +16,7 @@ public:
         }
 
         return (reversed == x);
-
-        //Method 2
-        if (x < 0 || (x != 0 && x % 10 == 0)) {
-            return false;
-        }
-
-        int reversed = 0;
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
-        }
-        return (x == reversed) || (x == reversed / 10);
-    
-
-
-        //Method 3
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
-        }
-        int revertedNumber = 0;
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
-            x /= 10;
-        }
-        return x == revertedNumber || x == revertedNumber / 10;
+        
     }
     
 };
